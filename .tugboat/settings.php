b2712dbd6817223e6973e2bf15c69c2095b4eb08
sime <simon@hobbs.id.au>
2019-09-30 09:34:05 +1000

    Show errors and be more permissive in general.

--- .tugboat/settings.php
+++ .tugboat/settings.php
@@ -30,3 +30,10 @@ $databases['default']['default'] = array (
 if (PHP_SAPI === 'cli') {
   ini_set('memory_limit', '256M');
 }
+
+$settings['trusted_host_patterns'] = ['.*'];
+
+$config['system.logging']['error_level'] = 'verbose';
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE);
+ini_set('display_startup_errors', TRUE);
